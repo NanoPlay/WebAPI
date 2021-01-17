93070c69b5c51cbb30f1c5f23a2fb592ee781abe
sebastiandoe5 <sebastiandoe5@gmail.com>
2021-01-17 10:03:28 +0000

    Create alpha app editor (there might be quite a lot of merging to do!

--- example/script.js
+++ example/script.js
@@ -20,6 +20,73 @@ function enable() {
     document.getElementById("removeTestAppButton").disabled = false;
 }
 
+function exportApp() {
+    download($("#appId").val() + ".nano.js", $(".codeinput").val());
+}
+
+// FORMAT FOR NM.JS FILE: ID||EN_NAME||FR_NAME
+
+function exportManifest() {
+    download($("#appId").val() + ".nm.js", $("#appId").val() + "||" + $("#name").val() + "||" + $("#nameFrench").val());
+}
+
+function uploadViaBluetooth() {
+    np.connect().then(function() {
+        np.uploadApp($('#code').val(), {
+            id: $('#appId').val(),
+            name: {
+                en_GB: $('#name').val(),
+                fr_FR: $('#nameFrench').val()
+            },
+            icon: testIcon
+        });
+    }).catch(function(error) {
+        console.error(error);
+    });
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+$(document).ready(function(){
+    $("#file").on('change',function(){
+        var fileInfo = $(this).prop('files')[0];
+        var fileName = fileInfo.name;
+        if (fileName.indexOf(".nano.js") >= 0) {
+            var fileReader = new FileReader();
+            fileReader.onload = function () {
+                var data = fileReader.result;
+                var coreData = data.replace('data:text/javascript;base64,','');
+                var codeOutput = window.atob(coreData);
+                $('.codeinput').val(codeOutput)
+                $('.importBackground').fadeOut();
+            };
+            fileReader.readAsDataURL($(this).prop('files')[0]);
+        } else if (fileName.indexOf(".nm") >= 0) {
+            var fileReader = new FileReader();
+            fileReader.onload = function () {
+                var data = fileReader.result;
+                var coreData = data.replace('data:application/octet-stream;base64,','');
+                var manifestOutput = window.atob(coreData);
+                var fullManifest = manifestOutput.split("||");
+                $("#name").val(fullManifest[1]);
+                $("#appNameInput").val(fullManifest[1]);
+                $("#nameFrench").val(fullManifest[2]);
+                $("#appId").val(fullManifest[0]);
+                $('.importBackground').fadeOut();
+            };
+            fileReader.readAsDataURL($(this).prop('files')[0]);
+        }
+    });
+});
+
 function uploadTestApp() {
     document.getElementById("status").innerText = "Connecting...";
     disable();
